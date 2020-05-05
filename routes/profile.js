7dbd7315ed8a0f90a72a17bb455b9e4d817bd688
aftermath089 <vincentmichael089@gmail.com>
2020-05-05 14:47:44 +0800

    add logging middleware

--- routes/profile.js
+++ routes/profile.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/:platform/:gamertag', (req,res)=>{
-    console.log(req.params.platform, req.params.gamertag)
-    res.send('hello')
+        console.log(req.params.platform, req.params.gamertag)
+        res.send('hello')
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const app = express()
 //middleware
 app.use('/api/v1/profile/', require('./routes/profile.js'))
 
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
 app.listen(8000, ()=>{
     console.log(`listening on port ${port}, running as ${process.env.NODE_ENV}`)
 })
