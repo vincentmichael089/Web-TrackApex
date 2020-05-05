f71b80f748b78cb3285a94396c027ae3910a9a86
aftermath089 <vincentmichael089@gmail.com>
2020-05-05 14:32:10 +0800

    add env variable

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
 
+dotenv.config({
+    path : './config.env'
+})
+
+const port = process.env.PORT || 8000
+
 const app = express()
 
 app.listen(8000, ()=>{
-    console.log(`listening on port `)
+    console.log(`listening on port ${port}, running as ${process.env.NODE_ENV}`)
 })
