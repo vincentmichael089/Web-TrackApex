d9826ee7b693ff11d5f359669aa8d1badbbe1673
aftermath089 <vincentmichael089@gmail.com>
2020-05-05 15:14:37 +0800

    fetch data from api

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,33 @@
 const express = require('express')
 const router = express.Router();
+const fetch = require('node-fetch')
 
-router.get('/:platform/:gamertag', (req,res)=>{
-        console.log(req.params.platform, req.params.gamertag)
-        res.send('hello')
+router.get('/:platform/:gamertag', async(req,res)=>{
+    try{
+        const headers = {
+            'TRN-Api-Key' : process.env.TRACKER_API_KEY
+        }
+        const platform = req.params.platform
+        const gamertag = req.params.gamertag
+
+        const response = await fetch(`${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`, {
+            headers : headers //response from server
+        })
+
+        const data = await response.json() 
+
+        if(data.errors && data.errors.length > 0){
+            return res.status(404).json({message:'Profile not found!'})
+        }
+
+        res.json(data) //response to client
+
+    }catch(err){
+        console.error(err)
+        res.status(500).json({
+            message: 'server error'
+        })
+    }
 })
 
 module.exports = router;
