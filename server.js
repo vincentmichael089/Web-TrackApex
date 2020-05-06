13e833c168bbf619bb549e037d0cb25821b57e14
aftermath089 <vincentmichael089@gmail.com>
2020-05-06 10:19:30 +0800

    production ready2

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ const app = express()
 //middleware
 app.use('/api/v1/profile/', require('./routes/profile.js'))
 
+//handle production 
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(__dirname+'/public/'))
+    //handle sigle page app
+    app.get(/.*/, (req, res) =>{ //if the path isnt available path, redirect to
+        res.sendFile(__dirname+"/public/index.html")
+    })
+}
+
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
