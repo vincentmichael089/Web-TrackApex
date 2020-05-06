1ee5792e849c4a77906feb677bf7fea6d9e83180
aftermath089 <vincentmichael089@gmail.com>
2020-05-06 10:00:25 +0800

    finish player profile

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -8,7 +8,23 @@
             <router-link to="/" class="btn">Back to Main Page</router-link>
         </div>
         <div v-if="profileData" class="container">
-            {{profileData.metadata.activeLegendName}}
+            <h1 class="gamertag">
+                <img :src="profileData.platformInfo.avatarUrl" alt="" class="platform-avatar">
+                {{profileData.platformInfo.platformUserId}}
+            </h1>
+            <div class="grid">
+                <div>
+                    <img :src="profileData.segments[1].metadata.imageUrl" alt="">
+                </div>
+                <div>
+                    <ul>
+                        <li>Selected Legend<p>{{profileData.metadata.activeLegendName}}</p></li>
+                        <li>Lifetime Kills<p>{{profileData.segments[0].stats.kills.displayValue}}</p><span>{{profileData.segments[0].stats.kills.percentile}}%</span></li>         
+                        <li>Damage Done<p>{{profileData.segments[0].stats.damage.displayValue}}</p><span>{{profileData.segments[0].stats.damage.percentile}}%</span></li>
+                    </ul>
+                </div>
+            </div>
+            <router-link to="/" class="btn">Back to Main Page</router-link>
         </div>
     </section>
 </template>
@@ -45,5 +61,76 @@ export default {
 </script>
 
 <style scoped>
-
-</style>>
+.container {
+  background: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  max-width: 700px;
+  margin: 1rem auto;
+  padding: 2rem 1.5rem;
+  border-radius: 20px;
+}
+h1.gamertag {
+  font-size: 2rem;
+  background: rgba(0, 0, 0, 0.6);
+  padding: 0.3rem 0.5rem;
+  text-align: center;
+  border-radius: 20px;
+  margin-bottom: 3rem;
+  display: flex;
+  align-items: center;
+}
+a {
+  display: inline-block;
+  margin-top: 2rem;
+  border: #fff 2px solid;
+  padding: 0.5rem 0.8rem;
+}
+a:hover {
+  border: #ccc 2px solid;
+  color: #ccc;
+}
+.platform-avatar {
+  width: 40px;
+  margin-right: 0.7rem;
+}
+img {
+  width: 100%;
+}
+.grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+}
+li {
+  background: rgba(0, 0, 0, 0.6);
+  padding: 1rem;
+  margin-bottom: 0.7rem;
+  border-radius: 10px;
+}
+li p {
+  font-size: 2rem;
+}
+li:first-child p {
+  font-size: 1.5rem;
+}
+li span {
+  font-size: 1rem;
+  color: #ccc;
+}
+@media (max-width: 500px) {
+  h1 {
+    font-size: 1.5rem;
+    display: block;
+    text-align: center;
+  }
+  .platform-avatar {
+    display: none;
+  }
+  .grid {
+    grid-template-columns: 1fr;
+  }
+  li p {
+    font-size: 1.5rem;
+  }
+}
+</style>
